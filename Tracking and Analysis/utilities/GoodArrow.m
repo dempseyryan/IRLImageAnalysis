c82893e016b9ed40ebc3e92b7c49a6df1c5dc59b
Ryan Dempsey <ryandempsey@cmail.carleton.ca>
2019-10-17 10:20:55 -0400

    Updated plotter, added frame step size to decrease jitter

--- Tracking and Analysis/utilities/GoodArrow.m
+++ Tracking and Analysis/utilities/GoodArrow.m
@@ -23,16 +23,17 @@ end
 if dilation ~= 0 
     d1 = xt1 - xt2;
     d2 = yt1 - yt2;
-    xt1 = xt1 + dilation*d1;
-    yt1 = yt1 + dilation*d2;
-
+    xt1 = xt1 + (dilation - 1)*d1;%dilation - 1 because when it is just dil'n
+    yt1 = yt1 + (dilation - 1)*d2;%will get different vector for 1x
+%%added term is just that - added, to the original
+%this version works for all values and is mathematically correct
 end
 if head == 1%if particle needs to be in the midpoint, change the head location to accomodate
     xt2 = 2*xt2 - xt1;
     yt2 = 2*yt2 - yt1;
-elseif head == 2%if it needs to be on the tail, change both the head and tail 
+elseif head == 2%if it needs to be on the tail, change both the head and tail to shift forward
     
-    newX = 2*xt2 - xt1;
+    newX = 2*xt2 - xt1;%current (xt2,yt2) is midpoint of desired (xt2,yt2) and current (xt1, yt1)
     newY = 2*yt2 - yt1;
     
     xt1 = xt2;
